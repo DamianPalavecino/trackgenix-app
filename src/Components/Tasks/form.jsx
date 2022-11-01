28a9fee853a131d1b72b002b0cd85534d25df676
mara666 <dario.sjs@gmail.com>
2022-11-01 16:21:05 -0300

    TG-10: add create functionality

--- src/Components/Tasks/form.jsx
+++ src/Components/Tasks/form.jsx
@@ -1,14 +1,68 @@
+import { useState } from 'react';
+import styles from './tasks.module.css';
+
 const Form = () => {
+  const [inputValue, setInputValue] = useState({
+    description: ''
+  });
+
+  const onChangeInput = (e) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+
+  const redirect = () => {
+    window.location.assign('/tasks');
+  };
+
+  const onSubmit = () => {
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(inputValue)
+    };
+    const url = `${process.env.REACT_APP_API_URL}/tasks`;
+    fetch(url, postOptions).then(async (response) => {
+      if (response.status !== 200 && response.status !== 201) {
+        const { message } = await response.json();
+        alert(message);
+      } else {
+        alert('Task was created successfully');
+        redirect();
+      }
+    });
+  };
   return (
-    <form>
-      <div>
-        <h3>Tasks</h3>
-      </div>
-      <div>
-        <label>Description</label>
-        <input type="text" name="name" />
-      </div>
-    </form>
+    <div className={styles.container}>
+      <h3>Tasks</h3>
+      <form>
+        <div>
+          <div>
+            <label>Description</label>
+          </div>
+          <div>
+            <input
+              className={styles.formInput}
+              type="text"
+              name="description"
+              value={inputValue.description}
+              onChange={onChangeInput}
+              placeholder="Task name"
+            />
+          </div>
+        </div>
+        <div>
+          <button type="button" onClick={redirect} className={styles.cancelButton}>
+            Cancel
+          </button>
+          <button type="button" onClick={onSubmit} className={styles.confirmButton}>
+            Upload
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
+
 export default Form;
