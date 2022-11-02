60b54f86d247010951e6327e35dd4663cf0d269c
Damian Palavecino <damianpalavecino25@gmail.com>
2022-11-02 00:31:18 -0300

    TG-6: Create Projects Form page to Create/Modify Projects

--- src/Components/Projects/projectForm.jsx
+++ src/Components/Projects/projectForm.jsx
@@ -1,21 +1,126 @@
+import { useState, useEffect } from 'react';
 import styles from './projectForm.module.css';
 
 const ProjectForm = () => {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get('id');
+  const url = `${process.env.REACT_APP_API_URL}/projects`;
+  const [inputValue, setInputValue] = useState({
+    name: '',
+    startDate: '',
+    endDate: '',
+    description: '',
+    clientName: ''
+  });
+  const fixDate = (date) => {
+    return date.slice(0, 10);
+  };
+
+  if (id) {
+    useEffect(async () => {
+      if (id) {
+        fetch(url + '/' + id)
+          .then((res) => res.json())
+          .then((data) =>
+            setInputValue({
+              name: data.data.name,
+              startDate: fixDate(data.data.startDate),
+              endDate: fixDate(data.data.endDate),
+              description: data.data.description,
+              clientName: data.data.clientName
+            })
+          );
+      }
+    }, []);
+  }
+
+  const onChangeInput = (e) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+
+  const redirect = () => {
+    window.location.assign('/projects');
+  };
+
+  const onSubmit = () => {
+    if (id) {
+      console.log(inputValue);
+      const put = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inputValue)
+      };
+      console.log(JSON.stringify(inputValue));
+      fetch(url + '/' + id, put).then(async (response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          const { message } = await response.json();
+          alert(message);
+        } else {
+          alert('Project was successfully edited');
+          redirect();
+        }
+      });
+    } else {
+      const post = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(inputValue)
+      };
+      fetch(url, post).then(async (response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          const { message } = await response.json();
+          alert(message);
+        } else {
+          alert('Project was created successfully');
+          redirect();
+        }
+      });
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h2>Add New Project</h2>
       <form className={styles.form}>
         <label>Project Name</label>
-        <input></input>
+        <input name="name" defaultValue={inputValue.name} onChange={onChangeInput}></input>
         <label>Description</label>
-        <input></input>
+        <textarea
+          name="description"
+          defaultValue={inputValue.description}
+          onChange={onChangeInput}
+        ></textarea>
         <label>Start Date</label>
-        <input></input>
+        <input
+          name="startDate"
+          type="date"
+          defaultValue={inputValue.startDate}
+          onChange={onChangeInput}
+        ></input>
         <label>End Date</label>
-        <input></input>
+        <input
+          name="endDate"
+          type="date"
+          defaultValue={inputValue.endDate}
+          onChange={onChangeInput}
+        ></input>
+        <label>Client</label>
+        <input
+          name="clientName"
+          defaultValue={inputValue.clientName}
+          onChange={onChangeInput}
+        ></input>
         <div>
-          <button className={styles.button}>Back</button>
-          <button className={styles.button}>Create</button>
+          <button className={styles.button} onClick={redirect}>
+            Back
+          </button>
+          <button className={styles.button} onClick={onSubmit}>
+            Upload
+          </button>
         </div>
       </form>
     </div>
