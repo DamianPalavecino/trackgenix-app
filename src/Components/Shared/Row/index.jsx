7d808072187fe695761bf868c33fffe491dccfbb
Damian Palavecino <damianpalavecino25@gmail.com>
2022-11-09 10:33:46 -0300

    TG-39: Modify header component to add callbacks inside of onClick functions and implement two validations for status and date

--- src/Components/Shared/Row/index.jsx
+++ src/Components/Shared/Row/index.jsx
@@ -2,17 +2,27 @@ import Button from '../Button';
 import styles from './row.module.css';
 
 const Row = ({ data, headers, handleDelete, editItem, showInfo }) => {
+  const fixDate = (date) => {
+    return date.slice(0, 10);
+  };
+
   return (
     <tr className={styles.row} key={data._id}>
       {headers.map((header, index) => {
         if (header === 'actions') {
           return (
             <td className={styles.td} key={index}>
-              <Button onClick={editItem} text="Edit" variant="editButton" />
-              <Button onClick={handleDelete} text="Delete" variant="confirmButton" />
+              <Button onClick={() => editItem(data._id)} text="Edit" variant="editButton" />
+              <Button
+                onClick={() => handleDelete(data._id)}
+                text="Delete"
+                variant="confirmButton"
+              />
             </td>
           );
         }
+        if (header === 'status') return <td key={index}>{data[header] ? 'Active' : 'Inactive'}</td>;
+        if (header.includes('Date')) return <td key={index}>{fixDate(data[header])}</td>;
         if (Array.isArray(data[header])) {
           return (
             <td key={index}>
