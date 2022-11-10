d0b165ec5a5b179fc7f3fd3249b3acdea2ee03bc
Maxi-SR <schmidtmaxi46@gmail.com>
2022-11-09 21:18:34 -0300

    TG-35: add prop to table and row so I can shows employees in a project

--- src/Components/Shared/Row/index.jsx
+++ src/Components/Shared/Row/index.jsx
@@ -1,13 +1,13 @@
 import Button from '../Button';
 import styles from './row.module.css';
 
-const Row = ({ data, headers, editItem, showInfo, handleDelete }) => {
+const Row = ({ data, headers, editItem, handleRelatedEntity, handleDelete }) => {
   return (
     <tr className={styles.row} key={data._id}>
       {headers.map((header, index) => {
         if (header === 'actions') {
           return (
-            <td className={styles.td} key={index}>
+            <td key={index}>
               <Button onClick={() => editItem(data._id)} text="Edit" variant="editButton" />
               <Button
                 onClick={() => {
@@ -19,14 +19,26 @@ const Row = ({ data, headers, editItem, showInfo, handleDelete }) => {
             </td>
           );
         }
+        if (header === 'status') return <td key={index}>{data[header] ? 'Active' : 'Inactive'}</td>;
+        if (header.includes('Date') && data[header])
+          return <td key={index}>{data[header].slice(0, 10)}</td>;
         if (Array.isArray(data[header])) {
           return (
             <td key={index}>
-              <Button onClick={showInfo} text={header} />
+              <Button
+                onClick={() => {
+                  handleRelatedEntity(data[header]);
+                }}
+                text={header}
+              />
             </td>
           );
         } else {
-          return <td key={index}>{data[header]}</td>;
+          return (
+            <td className={styles.td} key={index}>
+              {data[header]}
+            </td>
+          );
         }
       })}
     </tr>
