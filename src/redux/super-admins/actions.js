21989bd73d5b539a3ba7313e79411ba72a9020a6
mara666 <dario.sjs@gmail.com>
2022-11-14 23:35:57 -0300

    TG-56: create actions to delete a super admin

--- src/redux/super-admins/actions.js
+++ src/redux/super-admins/actions.js
@@ -1,4 +1,11 @@
-import { GET_ADMINS_PENDING, GET_ADMINS_FULFILLED, GET_ADMINS_REJECTED } from './constants';
+import {
+  GET_ADMINS_PENDING,
+  GET_ADMINS_FULFILLED,
+  GET_ADMINS_REJECTED,
+  DELETE_ADMINS_PENDING,
+  DELETE_ADMINS_FULFILLED,
+  DELETE_ADMINS_REJECTED
+} from './constants';
 
 export const getAdminsPending = () => {
   return {
@@ -16,6 +23,26 @@ export const getAdminsFulfilled = (data) => {
 export const getAdminsRejected = (error) => {
   return {
     type: GET_ADMINS_REJECTED,
+    payload: error
+  };
+};
+
+export const deleteAdminsPending = () => {
+  return {
+    type: DELETE_ADMINS_PENDING
+  };
+};
+
+export const deleteAdminsFulfilled = (data) => {
+  return {
+    type: DELETE_ADMINS_FULFILLED,
+    payload: data
+  };
+};
+
+export const deleteAdminsRejected = (error) => {
+  return {
+    type: DELETE_ADMINS_REJECTED,
     payload: error
   };
 };
