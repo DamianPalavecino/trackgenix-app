9d9b5a6d1873d6d989996820a50d54f76ccbcba9
Damian Palavecino <damianpalavecino25@gmail.com>
2022-11-18 20:19:35 -0300

    TG-8: Create actions, constants, reducer and thunks for Timesheets

---
+++ src/redux/timesheets/reducer.js
@@ -0,0 +1,117 @@
+import {
+  GET_TIMESHEETS_PENDING,
+  GET_TIMESHEETS_FULFILLED,
+  GET_TIMESHEETS_REJECTED,
+  DELETE_TIMESHEETS_PENDING,
+  DELETE_TIMESHEETS_FULFILLED,
+  DELETE_TIMESHEETS_REJECTED,
+  POST_TIMESHEETS_PENDING,
+  POST_TIMESHEETS_FULFILLED,
+  POST_TIMESHEETS_REJECTED,
+  PUT_TIMESHEETS_PENDING,
+  PUT_TIMESHEETS_FULFILLED,
+  PUT_TIMESHEETS_REJECTED,
+  GETBYID_TIMESHEETS_PENDING,
+  GETBYID_TIMESHEETS_FULFILLED,
+  GETBYID_TIMESHEETS_REJECTED
+} from './constants';
+
+const INITIAL_STATE = {
+  list: [],
+  isPending: false,
+  message: ''
+};
+
+const timeSheetReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case GET_TIMESHEETS_PENDING:
+      return {
+        ...state,
+        isPending: true
+      };
+    case GET_TIMESHEETS_FULFILLED:
+      return {
+        ...state,
+        list: action.payload,
+        isPending: false
+      };
+    case GET_TIMESHEETS_REJECTED:
+      return {
+        ...state,
+        isPending: false,
+        message: action.payload
+      };
+    case DELETE_TIMESHEETS_PENDING:
+      return {
+        ...state,
+        isPending: true
+      };
+    case DELETE_TIMESHEETS_FULFILLED:
+      return {
+        ...state,
+        list: [...state.list.filter((timesheet) => timesheet._id !== action.payload)],
+        isPending: false
+      };
+    case DELETE_TIMESHEETS_REJECTED:
+      return {
+        ...state,
+        isPending: false,
+        message: action.payload
+      };
+    case POST_TIMESHEETS_PENDING:
+      return {
+        ...state,
+        isPending: true
+      };
+    case POST_TIMESHEETS_FULFILLED:
+      return {
+        ...state,
+        isPending: false,
+        message: action.payload
+      };
+    case POST_TIMESHEETS_REJECTED:
+      return {
+        ...state,
+        isPending: false,
+        message: action.payload
+      };
+    case PUT_TIMESHEETS_PENDING:
+      return {
+        ...state,
+        isPending: true
+      };
+    case PUT_TIMESHEETS_FULFILLED:
+      return {
+        ...state,
+        isPending: false,
+        message: action.payload
+      };
+    case PUT_TIMESHEETS_REJECTED:
+      return {
+        ...state,
+        isPending: false,
+        message: action.payload
+      };
+    case GETBYID_TIMESHEETS_PENDING:
+      return {
+        ...state,
+        isPending: true
+      };
+    case GETBYID_TIMESHEETS_FULFILLED:
+      return {
+        ...state,
+        list: action.payload,
+        isPending: false
+      };
+    case GETBYID_TIMESHEETS_REJECTED:
+      return {
+        ...state,
+        isPending: false,
+        message: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default timeSheetReducer;
---
+++ src/redux/timesheets/thunks.js
@@ -0,0 +1,116 @@
+import {
+  getTimesheetsPending,
+  getTimesheetsFulfilled,
+  getTimesheetsRejected,
+  deleteTimesheetsPending,
+  deleteTimesheetsFulfilled,
+  deleteTimesheetsRejected,
+  postTimesheetsPending,
+  postTimesheetsFulfilled,
+  postTimesheetsRejected,
+  putTimesheetsPending,
+  putTimesheetsFulfilled,
+  putTimesheetsRejected,
+  getByIdTimesheetsPending,
+  getByIdTimesheetsFulfilled,
+  getByIdTimesheetsRejected
+} from './actions';
+
+export const getTimesheets = () => {
+  return async (dispatch) => {
+    dispatch(getTimesheetsPending());
+    fetch(`${process.env.REACT_APP_API_URL}/timesheets`)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.error) {
+          throw new Error(response.message);
+        } else {
+          dispatch(getTimesheetsFulfilled(response.data));
+        }
+      })
+      .catch((error) => {
+        dispatch(getTimesheetsRejected(error.toString()));
+      });
+  };
+};
+
+export const deleteTimesheets = (id) => {
+  return async (dispatch) => {
+    dispatch(deleteTimesheetsPending());
+    fetch(`${process.env.REACT_APP_API_URL}/timesheets/${id}`, {
+      method: 'DELETE'
+    })
+      .then((response) => {
+        if (response.status === 204) {
+          dispatch(deleteTimesheetsFulfilled(id));
+        }
+      })
+      .catch((error) => {
+        dispatch(deleteTimesheetsRejected(error.toString()));
+      });
+  };
+};
+
+export const postTimesheets = (newTimesheet) => {
+  return async (dispatch) => {
+    try {
+      dispatch(postTimesheetsPending());
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/timesheets/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTimesheet)
+      });
+      const data = await response.json();
+      if (response.ok) {
+        return dispatch(postTimesheetsFulfilled(data.message));
+      } else {
+        return dispatch(postTimesheetsRejected(data.message));
+      }
+    } catch (error) {
+      return dispatch(postTimesheetsRejected(error.toString()));
+    }
+  };
+};
+
+export const putTimesheets = (id, editedTimesheet) => {
+  return async (dispatch) => {
+    try {
+      dispatch(putTimesheetsPending());
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/timesheets/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(editedTimesheet)
+      });
+      const data = await response.json();
+      if (response.ok) {
+        return dispatch(putTimesheetsFulfilled(data.message));
+      } else {
+        return dispatch(putTimesheetsRejected(data.message));
+      }
+    } catch (error) {
+      return dispatch(putTimesheetsRejected(error.toString()));
+    }
+  };
+};
+
+export const getTimesheetsById = (id) => {
+  return async (dispatch) => {
+    dispatch(getByIdTimesheetsPending());
+    fetch(`${process.env.REACT_APP_API_URL}/timesheets/${id}`)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.error) {
+          throw new Error(response.message);
+        } else {
+          dispatch(getByIdTimesheetsFulfilled(response.data));
+        }
+      })
+      .catch((error) => {
+        dispatch(getByIdTimesheetsRejected(error.toString()));
+      });
+  };
+};
