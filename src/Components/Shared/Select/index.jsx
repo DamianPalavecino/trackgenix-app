9e58d651609b7989692956577465b556ee25f290
mara666 <dario.sjs@gmail.com>
2022-11-19 00:46:52 -0300

    TG-85: create Select shared component

---
+++ src/Components/Shared/Select/index.jsx
@@ -0,0 +1,45 @@
+import styles from './select.module.css';
+
+const Select = ({
+  selectName,
+  selectValue,
+  label,
+  optionValue,
+  error,
+  optionsData,
+  onChange,
+  item,
+  children
+}) => {
+  const handleChange = (e) => {
+    onChange(e);
+  };
+
+  return (
+    <div className={styles.divSelect}>
+      <label>{label}</label>
+      <select
+        className={styles.select}
+        name={selectName || ''}
+        value={selectValue || ''}
+        onChange={(e) => handleChange(e)}
+      >
+        <option selected disabled value="">
+          Select {optionValue}
+        </option>
+        {optionsData &&
+          optionsData.map((row) => {
+            return (
+              <option value={row._id} key={row._id}>
+                {row[item]}
+              </option>
+            );
+          })}
+        {children}
+      </select>
+      {error && <p className={styles.errorMessage}>{error}</p>}
+    </div>
+  );
+};
+
+export default Select;
