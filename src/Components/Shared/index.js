9ecb10ebd590f5efcdf48ec98d80803a7e36d035
Maxi-SR <schmidtmaxi46@gmail.com>
2022-11-20 20:28:27 -0300

    TG-70: add index into Shared folder to exports componentes

---
+++ src/Components/Shared/index.js
@@ -0,0 +1,9 @@
+import Button from './Button';
+import Input from './Input';
+import Modal from './Modal/Modal';
+import Select from './Select';
+import Row from './Row';
+import Spinner from './Spinner';
+import Table from './Table';
+
+export { Button, Input, Modal, Row, Spinner, Table, Select };
