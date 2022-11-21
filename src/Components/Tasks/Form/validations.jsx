393780deb7b6a332a1db3da7832edf587bb3ad3e
mara666 <dario.sjs@gmail.com>
2022-11-21 17:08:19 -0300

    TG-80: create a scheme using joi validations to import and use in the form

---
+++ src/Components/Tasks/Form/validations.jsx
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+
+export const taskSchema = Joi.object({
+  description: Joi.string()
+    .required()
+    .trim()
+    .min(3)
+    .max(100)
+    .pattern(/^(?=.[a-zA-Z].)([\w\s\W]+)$/)
+    .messages({
+      'string.empty': `"Description" can't be empty`,
+      'string.min': `"Description" must have minimum {3} characters`,
+      'string.max': `"Description" must have maximum {100} characters`,
+      'any.required': `"Description is required`,
+      'string.pattern.base': `"Description" can't have only numbers or special characters`
+    })
+});
