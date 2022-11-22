a89a63b455e38ad43d4f8033830275d7e7491c60
Damian Palavecino <damianpalavecino25@gmail.com>
2022-11-22 17:47:47 -0300

    TG-72: Create Employee Create Timesheet form

---
+++ src/Components/Employees/Timesheets/AddTimesheet/validations.jsx
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+export const timeSheetSchema = Joi.object({
+  description: Joi.string()
+    .required()
+    .trim()
+    .min(3)
+    .max(50)
+    .pattern(/^(?=.[a-zA-Z].)([\w\s\W]+)$/)
+    .messages({
+      'string.empty': '"Description" cannot be empty',
+      'string.min': '"Description" must have minimum {3} characters',
+      'string.max': 'Description" must have maximum {50} characters',
+      'any.required': '"Description" is required',
+      'string.pattern.base': '"Description" cannot have only numbers or special characters'
+    }),
+  hours: Joi.number().integer().min(1).positive().required(),
+  date: Joi.date().max('now').required(),
+  task: Joi.string().required(),
+  project: Joi.string().required()
+});
