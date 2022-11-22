c39714cb86a221bc56dd6c8c3caef48714213de8
Damian Palavecino <damianpalavecino25@gmail.com>
2022-11-22 17:45:09 -0300

    TG-72: Delete Projects Folder in Employees and create Home for Employees that show project list

--- src/Components/Employees/Home/index.jsx
+++ src/Components/Employees/Home/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import styles from './home.module.css';
+import { Table, Spinner } from 'Components/Shared';
+import { getByIdEmployees } from 'redux/employees/thunks';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const HomeEmployees = () => {
+  // const history = useHistory();
+  const { id } = useParams();
+  const [employeeProjects, saveProjects] = useState([]);
+  const dispatch = useDispatch();
+  const { list: employee, isPending } = useSelector((state) => state.employees);
+
+  useEffect(() => {
+    dispatch(getByIdEmployees(id));
+  }, []);
+
+  useEffect(() => {
+    saveProjects(employee?.projects);
+  }, [employee]);
+
   return (
     <div className={styles.container}>
-      <h2>Home Employee</h2>
+      <h2>Current Projects</h2>
+      {isPending ? (
+        <Spinner />
+      ) : (
+        <Table
+          headers={['name', 'startDate', 'endDate', 'description', 'clientName']}
+          data={employeeProjects}
+        />
+      )}
     </div>
   );
 };
