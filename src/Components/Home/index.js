6847597d7bbb69584a613a16b07f5bfeec2753dd
Damian Palavecino <damianpalavecino25@gmail.com>
2022-11-22 23:24:07 -0300

    TG-72: Fix Employees Home button not redirecting to employees home

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,13 +4,14 @@ import { useHistory } from 'react-router-dom';
 
 function Home() {
   const history = useHistory();
+  const employeeId = '637d055152a6ec59e69a46e3';
   return (
     <section className={styles.container}>
       <h2>Home</h2>
       <Button
         text="Go to employee home"
         variant="addButton"
-        onClick={() => history.push('/employees/home')}
+        onClick={() => history.push(`/employees/home/${employeeId}`)}
       />
     </section>
   );
