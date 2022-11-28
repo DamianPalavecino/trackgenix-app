9e1c683b8690450b721cf4a2969533372ddd4c04
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-11-28 20:54:57 -0300

    TG-92: test commit

--- src/redux/auth/thunks.js
+++ src/redux/auth/thunks.js
@@ -6,6 +6,7 @@ import { auth } from 'Components/Helpers/firebase';
 export const login = (inputData) => {
   return async (dispatch) => {
     dispatch(loginPending());
+    console.log('esto es una prueba');
     try {
       const userCredencials = await signInWithEmailAndPassword(
         auth,
