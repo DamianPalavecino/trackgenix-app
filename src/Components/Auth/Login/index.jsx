651f7f550a5aa2ace4a1f980febe4ce38c528376
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-11-28 14:59:08 -0300

    TG-92: add Layout to login and signup

--- src/Components/Auth/Login/index.jsx
+++ src/Components/Auth/Login/index.jsx
@@ -1,10 +1,13 @@
+import Layout from 'Components/Layout';
 import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Layout routes={[{ name: 'SignUp', path: '/auth/signup' }]}>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </Layout>
   );
 };
 
--- src/Components/Auth/SignUp/index.jsx
+++ src/Components/Auth/SignUp/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Layout from 'Components/Layout';
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <Layout routes={[{ name: 'Login', path: '/auth/login' }]}>
+      <div>
+        <h1>SignUp</h1>
+      </div>
+    </Layout>
   );
 };
 
