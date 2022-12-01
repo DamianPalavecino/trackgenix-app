efd67e14df3003468b3e7ab8f30d36830fc39092
Damian Palavecino <damianpalavecino25@gmail.com>
2022-12-01 16:56:36 -0300

    TG-97: Fix routes in Auth and Home component

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,51 +1,15 @@
 import { Button } from 'Components/Shared/';
 import styles from './home.module.css';
-import { useHistory } from 'react-router-dom';
 import Layout from 'Components/Layout';
-
-const routes = [
-  {
-    name: 'Home',
-    path: '/'
-  },
-  {
-    name: 'Admins',
-    path: '/admins'
-  },
-  {
-    name: 'Timesheets',
-    path: '/time-sheets'
-  },
-  {
-    name: 'Tasks',
-    path: '/tasks'
-  },
-  {
-    name: 'Employees',
-    path: '/employees'
-  },
-  {
-    name: 'Projects',
-    path: '/projects'
-  },
-  {
-    name: 'Super admins',
-    path: '/super-admins'
-  }
-];
+import { useHistory } from 'react-router-dom';
 
 function Home() {
   const history = useHistory();
-  const employeeId = '637e713b92952900f601204e';
   return (
-    <Layout routes={routes}>
+    <Layout routes={[]}>
       <section className={styles.container}>
         <h2>Home</h2>
-        <Button
-          text="Go to employee home"
-          variant="addButton"
-          onClick={() => history.push(`/employees/home/${employeeId}`)}
-        />
+        <Button text="Login pa" variant="addButton" onClick={() => history.push(`/auth/login`)} />
       </section>
     </Layout>
   );
--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -1,15 +1,24 @@
 import { Switch, Route, Redirect, useRouteMatch } from 'react-router-dom';
 import Login from 'Components/Auth/Login';
 import SignUp from 'Components/Auth/SignUp';
+import Layout from 'Components/Layout';
 
 const AuthRoutes = () => {
   const { url } = useRouteMatch();
   return (
-    <Switch>
-      <Route path={`${url}/login`} component={Login} />
-      <Route path={`${url}/signup`} component={SignUp} />
-      <Redirect to={`${url}/login`} />
-    </Switch>
+    <Layout
+      routes={[
+        { name: 'SignUp', path: '/auth/signup' },
+        { name: 'Login', path: '/auth/login' },
+        { name: 'Home', path: '/' }
+      ]}
+    >
+      <Switch>
+        <Route path={`${url}/login`} component={Login} />
+        <Route path={`${url}/signup`} component={SignUp} />
+        <Redirect to={`${url}/login`} />
+      </Switch>
+    </Layout>
   );
 };
 
