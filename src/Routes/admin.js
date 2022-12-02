40be1046f2f5f3d7d0ca0d7c5b9879f0f67e4009
Maxi-SR <schmidtmaxi46@gmail.com>
2022-12-01 23:26:05 -0300

    TG-97: add neccessary routes

--- src/Routes/admin.js
+++ src/Routes/admin.js
@@ -6,6 +6,7 @@ import Projects from 'Components/Projects';
 import ProjectsForm from 'Components/Projects/Form';
 import Tasks from 'Components/Tasks';
 import TaskForm from 'Components/Tasks/Form';
+import ProjectForm from 'Components/Projects/Form';
 import TimeSheets from 'Components/TimeSheets';
 import TimeSheetsForm from 'Components/TimeSheets/Form';
 import Layout from 'Components/Layout';
@@ -33,13 +34,24 @@ const Admin = () => {
     <Layout routes={routes}>
       <Switch>
         <Route exact path={'/admin/employees'} component={Employees} />
-        <Route exact path={'/admin/employees/form'} component={EmployeesForm} />
         <Route exact path={'/admin/projects'} component={Projects} />
         <Route exact path={'/admin/projects/form'} component={ProjectsForm} />
         <Route exact path={'/admin/tasks'} component={Tasks} />
         <Route exact path={'/admin/tasks/form'} component={TaskForm} />
+        <Route exact path={'/admin/tasks/delete/:id'} component={Tasks} />
+        <Route exact path={'/admin/tasks/form/:id'} component={TaskForm} />
         <Route exact path={'/admin/timesheets'} component={TimeSheets} />
         <Route exact path={'/admin/timesheets/form'} component={TimeSheetsForm} />
+        <Route exact path={'/admin/timesheets/delete/:id'} component={TimeSheets} />
+        <Route exact path={'/admin/timesheets/form/:id'} component={TimeSheetsForm} />
+        <Route exact path={'/admin/projects/:id/employees'} component={Projects} />
+        <Route exact path={'/admin/projects/delete/:id'} component={Projects} />
+        <Route exact path={'/admin/projects/form/:id'} component={ProjectForm} />
+        <Route exact path={'/admin/employees/form/:id'} component={EmployeesForm} />
+        <Route exact path={'/admin/employees/:id/projects'} component={Employees} />
+        <Route exact path={'/admin/employees/delete/:id'} component={Employees} />
+        <Route exact path={'/admin/employees/form'} component={EmployeesForm} />
+        <Route exact path={'/admin/employees/form/:id'} component={EmployeesForm} />
       </Switch>
     </Layout>
   );
