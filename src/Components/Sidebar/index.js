f897c9c0db2623de0306324db23a39375bf5e006
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-12-14 22:25:29 -0300

    create hamburger menu, add some styles with css and change body color

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -17,8 +17,11 @@ const Sidebar = (props) => {
       [modal]: !showModal[modal]
     });
   };
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <aside className={styles.side}>
+    <div className={styles.sidebarContainer}>
       <Modal
         showModal={showModal.success}
         variant="successModal"
@@ -29,27 +32,44 @@ const Sidebar = (props) => {
         title="You have logged out"
         text={'Redirecting to home page...'}
       ></Modal>
-      <ul className={styles.ul}>
-        {props?.routes?.routes?.map((route) => (
-          <li className={styles.li} key={route.name}>
-            <Link to={route.path} onClick={props.onClick}>
-              {route.name}
-            </Link>
-          </li>
-        ))}
-        {authenticated && (
-          <div className={styles.li}>
-            <Button
-              onClick={() => {
-                dispatch(logout());
-                toggleModal('success');
-              }}
-              text="Logout"
-            />
+      {isOpen && (
+        <div className={styles.divSidebar}>
+          <ul className={styles.ul}>
+            {props?.routes?.routes?.map((route) => (
+              <li className={styles.li} key={route.name}>
+                <Link to={route.path} onClick={props.onClick}>
+                  {route.name}
+                </Link>
+              </li>
+            ))}
+            {authenticated && (
+              <div className={styles.li}>
+                <Button
+                  onClick={() => {
+                    dispatch(logout());
+                    toggleModal('success');
+                  }}
+                  text="Logout"
+                />
+              </div>
+            )}
+          </ul>
+        </div>
+      )}
+      <button className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          <div className={styles.hamburger}>
+            <span className={styles.closeButton}>X</span>
+          </div>
+        ) : (
+          <div className={styles.hamburger}>
+            <div className={styles._layer}></div>
+            <div className={styles._layer}></div>
+            <div className={styles._layer}></div>
           </div>
         )}
-      </ul>
-    </aside>
+      </button>
+    </div>
   );
 };
 
