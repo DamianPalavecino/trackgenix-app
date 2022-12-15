c90601aa298671eb0c860a210fe3792b4af3231b
PilarFernandezC <pilarfernandezc98@gmail.com>
2022-12-15 16:35:01 -0300

    TG-110: implement mobile first on sidebar and change some styles

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Modal } from 'Components/Shared';
+import { Modal } from 'Components/Shared';
 import { logout } from 'redux/auth/thunks';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -32,6 +32,29 @@ const Sidebar = (props) => {
         title="You have logged out"
         text={'Redirecting to home page...'}
       ></Modal>
+      <div className={styles.divSidebarStatic}>
+        <ul className={styles.ul}>
+          {props?.routes?.routes?.map((route) => (
+            <li className={styles.li} key={route.name}>
+              <Link to={route.path} onClick={props.onClick}>
+                {route.name}
+              </Link>
+            </li>
+          ))}
+          {authenticated && (
+            <div className={styles.li}>
+              <a
+                onClick={() => {
+                  dispatch(logout());
+                  toggleModal('success');
+                }}
+              >
+                Logout
+              </a>
+            </div>
+          )}
+        </ul>
+      </div>
       {isOpen && (
         <div className={styles.divSidebar}>
           <ul className={styles.ul}>
@@ -44,13 +67,14 @@ const Sidebar = (props) => {
             ))}
             {authenticated && (
               <div className={styles.li}>
-                <Button
+                <a
                   onClick={() => {
                     dispatch(logout());
                     toggleModal('success');
                   }}
-                  text="Logout"
-                />
+                >
+                  Logout
+                </a>
               </div>
             )}
           </ul>
